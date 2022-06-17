eb135f9669dcb469695f7d809f1b44f95604001d
LuraMoth <fasreaperearnest@gmail.com>
2022-06-17 01:06:51 -0700

    get some of that movement action

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,16 +1,26 @@
 extends KinematicBody
 
+export var speed : int = 20
+var velocity : Vector3 = Vector3.ZERO
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func input_handler():
+	velocity = Vector3.ZERO
+	
+	# up
+	if Input.is_action_pressed("up"):
+		velocity.z += 1
+	# down
+	if Input.is_action_pressed("down"):
+		velocity.z -= 1
+	# left
+	if Input.is_action_pressed("left"):
+		velocity.x += 1
+	# right
+	if Input.is_action_pressed("right"):
+		velocity.x -= 1
+	# normalise velocity
+	velocity = velocity.normalized() * speed
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+func _physics_process(_delta):
+	input_handler()
+	velocity = move_and_slide(velocity)
