ba446e9b12137c81f15e63dd6d5cee892ff78729
LuraMoth <fasreaperearnest@gmail.com>
2022-06-17 18:35:01 -0700

    plop in some acceleration

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,26 +1,41 @@
 extends KinematicBody
 
-export var speed : int = 20
-var velocity : Vector3 = Vector3.ZERO
+#---------------- editables -------------------
+export var SPEED : int = 20
+export var FRICTION : float = 0.2
+export var ACCELERATION  : float = 0.2
 
-func input_handler():
-	velocity = Vector3.ZERO
+#---------------- no touchy -------------------
+var velocity : Vector3
+
+#---------------- functions -------------------
+# grabs axis inputs from WASD or controller
+func get_axis():
+	var axis : Vector3 = Vector3.ZERO
 	
 	# up
 	if Input.is_action_pressed("up"):
-		velocity.z += 1
+		axis.z += 1
 	# down
 	if Input.is_action_pressed("down"):
-		velocity.z -= 1
+		axis.z -= 1
 	# left
 	if Input.is_action_pressed("left"):
-		velocity.x += 1
+		axis.x += 1
 	# right
 	if Input.is_action_pressed("right"):
-		velocity.x -= 1
-	# normalise velocity
-	velocity = velocity.normalized() * speed
+		axis.x -= 1
+	
+	return axis.normalized()
 
+# function gets called every time the physics cycles
 func _physics_process(_delta):
-	input_handler()
+	var direction : Vector3 = get_axis()
+
+	# apply some friction and acceleration
+	if direction.length() > 0:
+		velocity = lerp(velocity, direction.normalized() * SPEED, ACCELERATION)
+	else:
+		velocity = lerp(velocity, Vector3.ZERO, FRICTION)
+	
 	velocity = move_and_slide(velocity)
